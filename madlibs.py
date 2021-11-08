1221627e39a224d7ccdb5ad990cc1af497147695
Shangyu Jiang <70888829+shangyuj@users.noreply.github.com>
2021-11-08 10:54:21 -0600

    lambda used

--- madlibs.py
+++ madlibs.py
@@ -3,4 +3,15 @@ import re
 pizza = open("pizza.txt", "r")
 pizza = pizza.read()
 
-taglist = re.findall(r"\[\[\w+]\]", pizza)
+taglist = re.findall(r"\[\[(.*?)\]\]", pizza)
+
+userwords = []
+
+for tag in taglist:
+	userword = input("Please enter a " + tag + ": ")
+	userwords.append(userword)
+
+word_list = iter(userwords)
+output = re.sub(r"\[\[\w+]\]", lambda L: next(word_list), pizza, 0, re.MULTILINE)
+
+print(output)
