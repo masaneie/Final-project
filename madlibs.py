b7c3bf3d64035b99f7aea011fbb435cb69c453f3
ViacheslavVovchenko <78502647+ViacheslavVovchenko@users.noreply.github.com>
2021-11-10 19:40:37 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -19,6 +19,7 @@ def takenext(x):
     return next(word_list)
 
 output = re.sub(r"\[\[(.+?)\]\]", takenext, madlib, 0, re.MULTILINE)
+output = re.sub(r'(a) ([aiueo])', r'an \2', output, flags=re.I|re.M)
 
 from datetime import *
 
