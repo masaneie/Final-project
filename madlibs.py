d34e963cc5bd8eb2591d9d938b33241dba961d89
ELATHAM1 <89711321+ELATHAM1@users.noreply.github.com>
2021-11-10 10:36:50 -0600

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,11 @@
 import re
 
-pizza = open("pizza.txt", "r")
-pizza = pizza.read()
+filename = input("Please type the filename with an extension: ")
 
-taglist = re.findall(r"\[\[(.+?)\]\]", pizza)
+madlib = open(filename)
+madlib = madlib.read()
+
+taglist = re.findall(r"\[\[(.+?)\]\]", madlib)
 
 userwords = []
 
@@ -16,6 +18,16 @@ word_list = iter(userwords)
 def takenext(x):
     return next(word_list)
 
-output = re.sub(r"\[\[(.+?)\]\]", takenext, pizza, 0, re.MULTILINE)
+output = re.sub(r"\[\[(.+?)\]\]", takenext, madlib, 0, re.MULTILINE)
 
-print(output)
+from datetime import *
+
+timestamp =str(datetime.now())
+timestamp = re.sub(r"\W", r"-", timestamp) 
+
+fileout = open(f'{filename[:-4]}_{timestamp[:-7]}.txt', "w")
+for line in output:
+    fileout.write(line)
+fileout.close()
+
+print(f'Your madlib is saved in {filename[:-4]}_{timestamp[:-7]}.txt')
